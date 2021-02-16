63f49945277f5a6161f3ccd8db700277e75d1d4a
umigame4031 <unun.octium1031@gmail.com>
2021-02-16 21:29:54 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :family_name, presence: true
-  validates :first_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  validates :password_confirmation, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
   validates :nickname, presence: true
-  validates :family_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
+  validates :family_name_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 end
