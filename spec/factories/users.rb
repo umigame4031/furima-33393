af7c9c3a3bf4d6b62b627ffeaf9dcfbdbcd0b88e
umigame4031 <unun.octium1031@gmail.com>
2021-02-20 12:22:52 +0900

    Rubocopによるコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     password { '111aaa' }
-    password_confirmation {password}
-    family_name { "稲葉" }
-    first_name { "浩志" }
-    nickname { "ビーズボーカル" }
-    email {Faker::Internet.free_email}
-    family_name_furigana { "イナバ" }
-    first_name_furigana { "コウシ" }
-    birthday { "2000-02-20" }
+    password_confirmation { password }
+    family_name { '稲葉' }
+    first_name { '浩志' }
+    nickname { 'ビーズボーカル' }
+    email { Faker::Internet.free_email }
+    family_name_furigana { 'イナバ' }
+    first_name_furigana { 'コウシ' }
+    birthday { '2000-02-20' }
   end
 end
