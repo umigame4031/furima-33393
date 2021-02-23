e63731caac43219a49decaa997f81834228a9d9f
umigame4031 <unun.octium1031@gmail.com>
2021-02-23 18:23:47 +0900

    商品画像添付機能の実装、出品機能のコントローラー

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :area, :category, :days, :delivery_fee, :state
+  belongs_to :area, :category, :days, :delivery_fee, :state
 
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :text
-    validates :price, numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :user
+    validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :state_id
